fdd3ebe71f0411bc632ca636bee7ff2a075fd7c4
Vishnu Sudheer <vishnusudheer1234@gmail.com>
2023-08-13 16:29:49 +0530

    Completed version

--- Presentation/presentation.js
+++ Presentation/presentation.js
@@ -1,102 +1,137 @@
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition(); 
-let array
-let n,p
-fetch('DATA.txt')
-  .then(response => response.text())
-  .then(text => {
+const SpeechRecognition =window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+let array;
+let n, p;
+fetch("DATA.txt")
+  .then((response) => response.text())
+  .then((text) => {
     // Parse the text as JSON to convert it into an array
     array = JSON.parse(text);
-    array = array.map(str => str.replace('D:/Mini-Project/Presentation/', ''));
-    array = array.map(str => str.replace("\\", '/'))
- 
+    array = array.map((str) =>
+      str.replace("D:/Mini-Project/Presentation/", "")
+    );
+    array = array.map((str) => str.replace("\\", "/"));
+
     console.log(array);
     //console.log(array[0])
     //console.log(array.length)
-    recognition.start()
+    recognition.start();
   })
-  .catch(error => {
-    console.error('Error:', error);
+  .catch((error) => {
+    console.error("Error:", error);
   });
 
 recognition.continuous = true;
 recognition.interimResults = false;
-const next = document.querySelector('.carousel-control-next');
-const prev =document.querySelector('.carousel-control-prev');
-let i=0  
-var img =document.getElementById('ki')
-var FS = document.getElementById('carouselExampleFade')
+const next = document.querySelector(".carousel-control-next");
+const prev = document.querySelector(".carousel-control-prev");
+let i = 0;
+var img = document.getElementById("ki");
+var FS = document.getElementById("carouselExampleFade");
+
+if(recognition.stop()){
+  recognition.start();
+}
 
 function handleClickN() {
-  if(FS.requestFullscreen){
-    FS.requestFullscreen()
+  if (FS.requestFullscreen) {
+    FS.requestFullscreen();
   }
-  if(p){
-    i=i+2
-    if(i>=array.length){
-      i=0
+  if (p) {
+    i = i + 2;
+    if (i >= array.length) {
+      i = 0;
     }
-    p=false
+    p = false;
   }
-  console.log('Button clicked next!');
-  img.setAttribute('style', 'content: url("'+array[i]+'")!important; height:100vh ;');
-  i++
-  if(i>=array.length){
-    i=0
+  console.log("Button clicked next!");
+  img.setAttribute(
+    "style",
+    'content: url("' + array[i] + '")!important; height:100vh ;'
+  );
+  i++;
+  if (i >= array.length) {
+    i = 0;
   }
-  if(i<0){
-    i=(array.length-1)
+  if (i < 0) {
+    i = array.length - 1;
   }
-  n = true
+  n = true;
 }
 
 function handleClickP() {
-  if(FS.requestFullscreen){
-    FS.requestFullscreen()
+  if (FS.requestFullscreen) {
+    FS.requestFullscreen();
   }
-  if(n){
-    i=i-2
-    if(i<0){
-      i=(array.length-1)
+  if (n) {
+    i = i - 2;
+    if (i < 0) {
+      i = array.length - 1;
     }
-    n=false
+    n = false;
   }
-  console.log('Button clicked prev!');
-  img.setAttribute('style', 'content: url("'+array[i]+'")!important; height:100vh ;');
-  i--
-  if(i<0){
-    i=(array.length-1)
+  console.log("Button clicked prev!");
+  img.setAttribute(
+    "style",
+    'content: url("' + array[i] + '")!important; height:100vh ;'
+  );
+  i--;
+  if (i < 0) {
+    i = array.length - 1;
   }
-  if(i>=array.length){
-    i=0
+  if (i >= array.length) {
+    i = 0;
   }
-  p = true
+  p = true;
 }
 
-recognition.addEventListener('result', (e) => {
-    var text = Array.from(e.results)
-      .map(result => result[0])
-      .map(result => result.transcript)
-      .pop()
+recognition.addEventListener("result", (e) => {
+  var text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .pop();
+  console.log(text);
 
-    console.log(text)
-    a = text.endsWith("next")
-    b = text.endsWith("previous")
+
+  a = text.endsWith("next");
+  b = text.endsWith("previous");
+
+  if(text.indexOf("go to") !=  -1)
+    c = true
+  else
+    c=false
+  
+  const match = text.match(/(\d+)$/);
+  
+  if (a) {
+    console.log("yes");
+    next.click();
+    a = false;
+  }
+
+  if (b) {
+    console.log("no");
+    console.log(i);
+    prev.click();
+    b = false;
+  }
  
+    if(c && match) {
 
-    if(a){
-      console.log("yes");
-      next.click();
-      a = false;
-    } 
-     
-    if(b){
-      console.log("no");
-      console.log(i)
-      prev.click(); 
-      b = false;
-    }
-    
-    })
-    
+        var number = match[1];
+        console.log("The word ends with the number: " + number);
+        number = parseInt(number)
+        if(number < 1 || number > array.length-1){
+          alert("Slide does not exist")
+        }
+        else{
+          i=number-1
+          img.setAttribute(
+            "style",
+            'content: url("' + array[i] + '")!important; height:100vh ;'
+          );
+        }
+  }
+});
 
--- upload.js
+++ upload.js
@@ -1,3 +1,4 @@
+//Frame works required 
 var convertapi = require("convertapi")("MK85H7rYV8qiA2VA", {
   conversionTimeout: 60,
   uploadTimeout: 60,
@@ -7,8 +8,8 @@ const express = require("express");
 const app = express();
 const multer = require("multer");
 const fs = require("fs");
-const { request } = require("http");
 
+//Used to store ppt in local system
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./Presentation/ppt"); // Specify the directory to which the file will be uploaded
@@ -20,17 +21,20 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 app.use(express.urlencoded({ extended: false }));
-
 app.use("/", express.static(__dirname + "/Upload"));
+
+//Home page
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/Upload/upload.html");
 });
+
 
 app.post("/", upload.single("myFile"), (req, res) => {
   // Access the uploaded file using req.file
   console.log(req.file);
   console.log(req.file.originalname);
 
+  // Coverts ppt to png form
   convertapi
     .convert("png", {
       File: "D:/Mini-Project/Presentation/ppt/" + req.file.originalname,
@@ -44,6 +48,8 @@ app.post("/", upload.single("myFile"), (req, res) => {
         .saveFiles("D:/Mini-Project/Presentation/png")
         .then(function (files) {
           console.log("Files saved: " + files);
+
+          //To write the pngs location into DATA.txt
           fs.writeFile('./Presentation/DATA.txt', JSON.stringify(files), function (err) {
            if (err) throw err;
            console.log('File is created successfully.');
@@ -56,25 +62,28 @@ app.post("/", upload.single("myFile"), (req, res) => {
       console.error(e.toString());
     });
 
-
+//When submit button is clicked goes to loading state
 return res.redirect("/loading");
 });
 
 
  
 //loading
-
 app.get("/loading",(req,res)=>{
+  //Buffering for 5 sec
   setTimeout(() => {
+    //Goes to presentation page
     return res.redirect("/presentation");
   }, 5000);
 })
 
 app.use("/presentation", express.static(__dirname + "/Presentation"));
+//Presentation Page
 app.get("/presentation", (req, res) => {
   res.sendFile(__dirname + "/Presentation/presentation.html");
 });
 
+//Listens to port 3000 where the site exists
 app.listen(3000 || process.env.PORT, () => {
   console.log("Server is running on port 3000");
 });
